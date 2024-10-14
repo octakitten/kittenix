8ce2c72d90cf1a5723675711274f745b3b7a655e
octakitten <raspberry2425@gmail.com>
2024-10-14 18:07:52 -0400

    fixing git import

--- bootstrap/configuration.nix
+++ bootstrap/configuration.nix
@@ -96,7 +96,7 @@ in
     description = "octa";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-      thunderbird
+      # thunderbird
     ];
   };
 
@@ -119,7 +119,7 @@ nix.settings.experimental-features = [ "nix-command" "flakes" ];
   environment.systemPackages = with pkgs; [
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
-    git
+    gitAndTools.gitFull
    #  unstable.parsec-bin
   ];
 
